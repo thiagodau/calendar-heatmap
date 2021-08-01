6ff9a514fd1efceb27215c0c189a9daf2917fd35
Thiago Dau <thiagorodriguesdau@gmail.com>
2021-08-01 17:22:23 -0400

    added firebase and features of button to mark day studied is done

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 ## Calendário Heatmap com React JS
+
 <p> A ideia é fazer um calendário como o do github, para marcar os dias que eu estudar inglês. </p>
 <br />
 <p> Exemplo de Calendário </p>
@@ -6,14 +7,13 @@
 <br />
 
 <h2>Tecnologias</h2>
-<p> Estou utilizando MomentJs para formatar as datas e React Calendar Heatmap para consumir o calendário do repositório. </p>
+<p> Estou utilizando MomentJs para formatar as datas e React Calendar Heatmap para consumir o calendário do repositório, também utilizei o firebase para o backend, estou utilizando o database e por fim o hosting para hospedar a aplicação. </p>
 
 <ul>
   <li>Moment JS <a href="https://momentjs.com/" target="_blank">Clique aqui</a></li>
   <li>React Calendar Heatmap <a href="https://raw.githubusercontent.com/patientslikeme/react-calendar-heatmap/HEAD/demo/public/react-calendar-heatmap.png?raw=true" target="_blank">Clique aqui</a></li>
+  <li>Firebase <a href="https://firebase.google.com/?hl=pt" target="_blank">Clique aqui</a></li>  
 </ul>
 
-    
-<br/> :email: &nbsp; Contato comigo: [![Linkedin Badge](https://img.shields.io/badge/-Thiago-blue?style=flat-square&logo=Linkedin&logoColor=white&link=https://www.linkedin.com/in/thiagorodriguesdau/)](https://www.linkedin.com/in/thiagorodriguesdau/) 
+<br/> :email: &nbsp; Contato comigo: [![Linkedin Badge](https://img.shields.io/badge/-Thiago-blue?style=flat-square&logo=Linkedin&logoColor=white&link=https://www.linkedin.com/in/thiagorodriguesdau/)](https://www.linkedin.com/in/thiagorodriguesdau/)
 [![Gmail Badge](https://img.shields.io/badge/-thiagorodriguesdau@gmail.com-c14438?style=flat-square&logo=Gmail&logoColor=white&link=mailto:thiagorodriguesdau@gmail.com)](mailto:thiagorodriguesdau@gmail.com)
-
--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import moment from 'moment'
+import { useEffect, useState } from 'react'
+import CalendarHeatmap from 'react-calendar-heatmap'
+import 'react-calendar-heatmap/dist/styles.css'
+
+import { database } from '../src/services/firebase'
+
+import './App.css'
 
 function App() {
+  const [datas, setDatas] = useState([
+    //{ date: '2021-07-29' },
+    //{ date: '2021-07-30' }
+  ])
+
+  const [exist, setExist] = useState()
+
+  useEffect(() => {
+    const datesReef = database.ref('dates')
+
+    datesReef.on('value', data => {
+      const databaseDate = data.val() //object
+      const allDates = []
+
+      try {
+        Object.entries(databaseDate).map(([id, object]) => {
+          allDates.push({ date: object.date })
+          if (object.date === dateOfDay()) {
+            setExist(true)
+          } else {
+            setExist(false)
+          }
+        })
+        setDatas(allDates)
+      } catch {
+        alert(
+          'Se esta vendo isso é por que no BD não tem registro de datas, então vou adicionar agora uma data no banco (2021-07-31)'
+        )
+        datesReef.push({
+          date: '2021-07-31'
+        })
+      }
+    })
+  }, [exist])
+
+  function dateOfDay() {
+    let date = new Date()
+    let dateFormated = moment(date).format('YYYY-MM-DD') //YYYY-MM-DD
+    return dateFormated
+  }
+
+  async function addedDate() {
+    const datesReef = database.ref('dates')
+
+    if (exist === false) {
+      await datesReef.push({
+        date: dateOfDay()
+      })
+      setExist(true)
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Historic of Study English Language per day</h1>
+      <p>Data incio: 01/07/2021 | Data Fim: 30/04/2022</p>
+
+      <div className="button">
+        {exist ? (
+          <h3>Parabéns por ter estudado hoje!</h3>
+        ) : (
+          <button onClick={addedDate}>Estudou hoje ?</button>
+        )}
+      </div>
+
+      <div className="calendar">
+        <CalendarHeatmap
+          horizontal
+          showOutOfRangeDays={false}
+          showWeekdayLabels
+          startDate={new Date('2021-07-01')}
+          endDate={new Date('2022-04-30')}
+          values={datas}
+        />
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
